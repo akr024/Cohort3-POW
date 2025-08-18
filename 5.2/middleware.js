6846cb274625c1f67f80e2ab63ff4f3bff0df3c7
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-08-17 22:45:54 -0700

    frontend access through cors library

--- 5.2/middleware.js
+++ 5.2/middleware.js
@@ -1,47 +1,25 @@
-// small assignment: create a middleware to log the each req's http method, url and timestamp
+// small assignment: create a small backend for computing - utilising cors
 
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+// to parse the JSON of the body of the req:
+app.use(express.json());
+app.use(cors({
+    domain: "" // begin server on your frontend seperately using npx serve
+}));
 
-let requestCount = 0;
+app.post('/add', (req, res) => {
+    console.log(req.body);
+    const a = parseInt(req.body.a);
+    const b = parseInt(req.body.b);
 
-function logMWFunc(req, res, next){
-    requestCount++;
-    console.log("Request number:", requestCount);
-    console.log("Method: " + req.method);
-    console.log("URL:", req.hostname); // req.url contains route, req.hostname contains the full rul
-    const time = new Date;
-    console.log("Timestamp", time);
+    const ans = a+b;
 
-    next();
-}
-
-// middleware:
-app.use(logMWFunc);
-
-app.get('/multiply', (req, res) => {
-    const a = req.query.a;
-    const b = req.query.b;
-
-    console.log(a);
-    console.log(b);
-
-    console.log(a*b);
-
-    res.send(a*b);
-})
-
-app.get('/add', (req, res) => {
-    const a = (Number) (req.query.a);
-    const b = (Number) (req.query.b);
-
-    console.log(a);
-    console.log(b);
-
-    console.log(a+b);
-
-    res.send((a+b));
+    res.json({
+        "sum": ans
+    })
 })
 
 app.listen(3000, function(){
