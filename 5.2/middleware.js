99b12b40d06fc5352939a8c1d51b80d6a203b92c
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-08-17 18:29:12 -0700

    middleware basic assignment

---
+++ 5.2/middleware.js
@@ -0,0 +1,51 @@
+// small assignment: create a middleware to log the each req's http method, url and timestamp
+
+const express = require('express');
+
+const app = express();
+
+let requestCount = 0;
+
+app.use(express.json());
+
+function logMWFunc(req, res, next){
+    requestCount++;
+    console.log("Request number:", requestCount);
+    console.log("Method: " + req.method);
+    console.log("URL: http://localhost:3000/", req.url);
+    const time = new Date;
+    console.log("Timestamp", time);
+
+    next();
+}
+
+// middleware:
+app.use(logMWFunc);
+
+app.get('/multiply', (req, res) => {
+    const a = req.query.a;
+    const b = req.query.b;
+
+    console.log(a);
+    console.log(b);
+
+    console.log(a*b);
+
+    res.send(a*b);
+})
+
+app.get('/add', (req, res) => {
+    const a = (Number) (req.query.a);
+    const b = (Number) (req.query.b);
+
+    console.log(a);
+    console.log(b);
+
+    console.log(a+b);
+
+    res.send((a+b));
+})
+
+app.listen(3000, function(){
+    console.log("server running");
+})
