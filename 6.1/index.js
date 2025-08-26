d801df9e91f80e782de5994b91d4e43d246f531b
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-08-25 17:04:36 -0700

    base for token-based server setup

---
+++ 6.1/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const users = [];
+
+app.use(express.json());
+
+function generateToken(){
+    
+}
+
+app.post('/signup', (req,res) => {
+
+})
+
+app.post('/signin', (req,res) => {
+    
+})
