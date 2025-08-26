9cb1ae1be8ff66f0c75accb1500c5f9fa10ce056
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-08-25 19:16:34 -0700

    basic token generation on signin/signup

--- 6.1/index.js
+++ 6.1/index.js
@@ -7,15 +7,58 @@ const users = [];
 app.use(express.json());
 
 function generateToken(){
-
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+    let token = "";
+    for(let i = 0; i < 7; i++){
+        token += letters[Math.floor(Math.random() * 6)]
+    }
+    return token;
 }
 
 app.post('/signup', (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    if(users.find(function(user){
+        if (user.username === username) {
+            return true};
+    })){
+        res.json({message: "You are already signed up"})
+    }
+
+    users.push({
+        username: username,
+        password: password
+    })
+    res.json({
+        message: "user signed up"
+    })
+    console.log(users);
 })
 
 app.post('/signin', (req,res) => {
-    
+    const username = req.body.username;
+    const password = req.body.password;
+    const user = users.find(function (u) {
+        if(u.username === username && u.password === password){
+            return true;
+        } else{
+            return false;
+        }
+    })
+
+    if (user) {
+        const token = generateToken();
+        user.token = token;
+        res.json({
+            token: token
+        })
+    } else {
+        res.status(404).json({
+            message: "User does not exist"
+        })
+    }
+
 })
 
 app.listen(3000, function(){
