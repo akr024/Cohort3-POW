9b48039c9253af1d23a19cd8f0c5fd82086487e2
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-11-10 15:28:29 -0800

    error boundary

--- 9.4/react-practice/src/App.jsx
+++ 9.4/react-practice/src/App.jsx
@@ -4,21 +4,48 @@ import React from 'react'
 function App() {
 
   return (
+    // fragments <> </>
     <>
-      <Card>
-        Hello I am a child
-      </Card>
+      <ErrorBoundary>
+        <Card>
+          Hello I am a child
+        </Card>
+      </ErrorBoundary>
     </>
   )
 }
 
 function Card({children}){
 
+  throw new Error("Something went wrong")
+
   return(
     <div style={{color: "red"}}>
       {children}
     </div>
   )
+}
+
+// error boundary black box code
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
 }
 
 
