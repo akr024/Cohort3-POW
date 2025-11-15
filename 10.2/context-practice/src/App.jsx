6c5426f24d03d9f712f1320d27f532687a0be9f6
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-11-14 17:45:20 -0800

    prop-based state management

---
+++ 10.2/context-practice/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+
+function App() {
+
+  const [lightStat, setLightStat] = useState(true);
+
+  return (
+    <>
+      <LightBulb lightStat={lightStat} setLightStat={setLightStat}/>
+    </>
+  )
+}
+
+
+function LightBulb({lightStat, setLightStat}){
+  return (
+    <>
+      <LightStatus lightStat={lightStat} />
+      <ToggleButton setLightStat={setLightStat}/>
+    </>
+  )
+}
+
+function ToggleButton({setLightStat}){
+
+  function switchLight(){
+    setLightStat(c => !c)
+  }
+
+  return(
+    <div>
+      <button onClick={switchLight}>Toggle Light</button>
+    </div>
+  )
+}
+
+function LightStatus({lightStat}){
+
+  return (
+    <div>
+      {lightStat ? "ON!" : "OFF!"}
+    </div>
+  )
+}
+
+
+export default App
