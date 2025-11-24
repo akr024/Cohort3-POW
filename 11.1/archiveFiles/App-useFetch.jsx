5fe4673b02a0c7faa03aa6c98f3dd97bdce46a7c
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-11-24 01:08:30 -0800

    implemented usePrev

---
+++ 11.1/archiveFiles/App-useFetch.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+
+
+function useFetch(url, interval){
+  const [posts, setPosts] = useState({});
+  const [loading, setLoading] = useState(true)
+
+  async function getPost(){
+    setLoading(true)
+    const postsData = await fetch(url);
+    const posts = await postsData.json();
+    setPosts(posts);
+    setLoading(false)
+  }
+
+  useEffect(function() {
+    const inter = setInterval(function(){
+      getPost();
+      console.log("refetched")
+    }, interval)
+
+    return (function(){
+      clearInterval(inter)
+    })
+  }, [])
+
+
+  useEffect(function(){
+    getPost();
+  }, [url])
+
+  return {posts, loading};
+
+}
+
+
+function App() {
+  const [url, setUrl] = useState('https://jsonplaceholder.typicode.com/todos/1')
+  
+  const {posts, loading} = useFetch(url, 3000)
+
+  function getData(num){
+    setUrl('https://jsonplaceholder.typicode.com/todos/' + num)
+  }
+
+
+  return (
+    <>
+      <div>
+        <div>
+          {loading ? "Loading..": posts.title}
+        </div>
+        <button onClick={()=> getData(1)}>Get 1</button>
+        <button onClick={()=> getData(2)}>Get 2</button>
+        <button onClick={()=> getData(3)}>Get 3</button>
+      </div>
+    </>
+  )
+}
+
+export default App
--- 11.1/customhooks/src/App.jsx
+++ 11.1/customhooks/src/App.jsx
@@ -1,57 +1,28 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
+function usePrev(num){
+  let previous = useRef(null);
 
-function useFetch(url, interval){
-  const [posts, setPosts] = useState({});
-  const [loading, setLoading] = useState(true)
-
-  async function getPost(){
-    setLoading(true)
-    const postsData = await fetch(url);
-    const posts = await postsData.json();
-    setPosts(posts);
-    setLoading(false)
-  }
-
-  useEffect(function() {
-    const inter = setInterval(function(){
-      getPost();
-    }, interval)
-
-    return (function(){
-      clearInterval(inter)
-    })
-  }, [])
-
-
+  // useEffect runs after returning previous.current on line 11
   useEffect(function(){
-    getPost();
-  }, [url])
+    previous.current = num
+  }, [num])
 
-  return {posts, loading};
-
+  return previous.current;
 }
 
 
 function App() {
-  const [url, setUrl] = useState('https://jsonplaceholder.typicode.com/todos/1')
-  
-  const {posts, loading} = useFetch(url, 3000)
-
-  function getData(num){
-    setUrl('https://jsonplaceholder.typicode.com/todos/' + num)
-  }
-
+  const [count, setCount] = useState(0);
+  const result = usePrev(count);
+  console.log(result)
 
   return (
     <>
       <div>
-        <div>
-          {loading ? "Loading..": posts.title}
-        </div>
-        <button onClick={()=> getData(1)}>Get 1</button>
-        <button onClick={()=> getData(2)}>Get 2</button>
-        <button onClick={()=> getData(3)}>Get 3</button>
+        {count}
+        <button onClick={() => {setCount(c=>c+1)}}>increase</button>
+        previous value: {result}
       </div>
     </>
   )
