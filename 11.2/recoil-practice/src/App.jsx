cbc326a23b2e9117d83f2d3cf0fcf2e8b81c64dd
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-11-24 17:12:44 -0800

    revision of context api with counter app

--- 11.2/recoil-practice/src/App.jsx
+++ 11.2/recoil-practice/src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const appContext = createContext();
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <appContext.Provider value={{count, setCount}}>
+        <Increase />
+        <Decrease />
+        <Value />
+      </appContext.Provider>
+      
     </>
+  )
+}
+
+
+function Increase(){
+  const {setCount} = useContext(appContext);
+
+  return (
+    <div>
+      <button onClick={() => {setCount(c => c+1)}}>Increase</button>
+    </div>
+  )
+}
+
+
+function Decrease(){
+  const {setCount} = useContext(appContext);
+
+  return (
+    <div>
+      <button onClick={() => {setCount(c => c-1)}}>Decrease</button>
+    </div>
+  )
+}
+
+
+function Value(){
+  const {count} = useContext(appContext);
+
+  return (
+    <div>
+      {count}
+    </div>
   )
 }
 
