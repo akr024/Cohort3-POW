6d09b6f198bd924fee8cc520e185bb690a0a4a28
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-12-25 02:15:10 -0800

    readonly api

--- 14.3/src/index.ts
+++ 14.3/src/index.ts
@@ -16,18 +16,44 @@
 
 // Partial API
 
-type user = {
-    age: number,
-    address: {street: string, aptNo: number}
-}
-
-type OptionalUser = Partial<user> // now, all fields in user are optional
-
-const optUser: OptionalUser = {
-    age: 3
-}
-
-// below is not allowed:
-// const mandUser: user = {
-//     age: 44,
+// type user = {
+//     age: number,
+//     address: {street: string, aptNo: number}
 // }
+
+// type OptionalUser = Partial<user> // now, all fields in user are optional
+
+// const optUser: OptionalUser = {
+//     age: 3
+// }
+
+// // below is not allowed:
+// // const mandUser: user = {
+// //     age: 44,
+// // }
+
+// Readonly API
+
+type dataObj = {
+    readonly name: string,
+    readonly info: boolean
+} // now, once an obj is initialised, name and info are unchangeable
+
+const data: dataObj = {
+    name: "someName",
+    info: true
+}
+
+// OR
+
+type dataObj2 = {
+    name: string,
+    info: boolean
+}
+
+const data2: Readonly<dataObj2> = {
+    name: "someName",
+    info: true
+}
+
+// data2.name = "someothername" // not allowed anymore, diff syntax
