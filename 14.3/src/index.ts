b254bb5d076dc2ca5c6f77c2d7516c38c41389a7
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-12-25 02:11:06 -0800

    Partial API

--- 14.3/src/index.ts
+++ 14.3/src/index.ts
@@ -1,16 +1,33 @@
 // API Pick
 
-type UserType = {
-    name: string,
+// type UserType = {
+//     name: string,
+//     age: number,
+//     address: string,
+//     ssn: string
+// }
+
+// type UserInfo = Pick<UserType, 'name' | 'age'>
+
+// const user1: UserInfo = {
+//     name: "user1name",
+//     age: 29
+// }
+
+// Partial API
+
+type user = {
     age: number,
-    address: string,
-    ssn: string
+    address: {street: string, aptNo: number}
 }
 
-type UserInfo = Pick<UserType, 'name' | 'age'>
+type OptionalUser = Partial<user> // now, all fields in user are optional
 
-const user1: UserInfo = {
-    name: "user1name",
-    age: 29
+const optUser: OptionalUser = {
+    age: 3
 }
 
+// below is not allowed:
+// const mandUser: user = {
+//     age: 44,
+// }
